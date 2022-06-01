0d144f58449b3389dd91cfde2af126f70d6555ad
Dominika Jadowska <domi.jadowska@gmail.com>
2022-06-01 18:49:38 +0200

    Enable NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <!-- <navBar /> -->
+    <navBar />
     <router-view />
   </div>
 </template>
 
 //
 <script>
-// import navBar from "@/components/NavBar.vue"
-// export default {
-//   components: {
-//     navBar
-//   }
-// }
-//
+import navBar from "@/components/NavBar.vue";
+export default {
+  components: {
+    navBar,
+  },
+};
 </script>
 
 <style lang="scss">
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,28 +1,14 @@
 <template>
   <div>
     <nav>
-      <div class="title">Live-Action Map</div>
+      <img src="/icon.png" />
       <div class="links">
         <router-link class="link" to="/">Map</router-link>
         <router-link class="link" to="/feed">Feed</router-link>
         <router-link class="link" to="/about">About</router-link>
       </div>
-      <div class="refresh">
-        <i v-if="refreshStatus" class="fa-solid fa-arrows-rotate"></i>
-        <i v-else class="fa-solid fa-arrows-rotate fa-spin"></i>
-      </div>
     </nav>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      refreshStatus: true,
-    };
-  },
-};
-</script>
 
 <style lang="scss" scoped></style>
