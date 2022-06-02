e9ec1e6d2bc66ced792e814c40d1ae8e51be86a7
Dominika Jadowska <domi.jadowska@gmail.com>
2022-06-02 14:37:46 +0200

    Add contributors

---
+++ src/components/ContributorComponent.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="contributor">
+    <div class="image">
+      <img :src="contributor.image" alt="" />
+    </div>
+    <div class="body">
+      <div class="name">{{ contributor.name }}</div>
+      <div class="roles">
+        <span
+          class="role"
+          v-for="(role, index) in contributor.roles"
+          :key="index"
+          >{{ role }}</span
+        >
+      </div>
+      <div class="description">Lead developer v2.0 up</div>
+      <div class="socials">
+        <a
+          v-for="(social, index) in contributor.socials"
+          :key="index"
+          :style="`color: ${social.color}`"
+          class="social"
+          :href="social.link"
+          target="_blank"
+          ><i :class="social.icon"></i
+        ></a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    contributor: Object,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.contributor {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-width: 300px;
+  .image {
+    background-color: purple;
+    border-radius: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    outline: 10px whitesmoke solid;
+    z-index: 2;
+    img {
+      height: 150px;
+      background-color: white;
+      border-radius: 50%;
+      margin: 5px;
+    }
+  }
+  .body {
+    background-color: #90dfaa;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    z-index: 0;
+    position: relative;
+    top: -20px;
+    border-radius: 15px;
+    padding: 10px;
+    .name {
+      padding-top: 25px;
+      font-size: 25px;
+    }
+  }
+}
+
+.socials {
+  font-size: 25px;
+}
+
+.social:hover {
+  text-shadow: 2px 2px 2px rgb(121, 121, 121);
+}
+
+.roles,
+.socials {
+  display: flex;
+  gap: 5px;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,9 +1,69 @@
 <template>
-  <div></div>
+  <div class="about">
+    <div class="content">
+      <div class="holder">
+        <div class="section-title">Contributors:</div>
+        <div class="section">
+          <contributor
+            v-for="(contributor, index) in contributors"
+            :key="index"
+            :contributor="contributor"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import contributor from "@/components/ContributorComponent.vue";
+
+import axios from "axios";
+export default {
+  components: {
+    contributor,
+  },
+  data() {
+    return {
+      contributors: [],
+    };
+  },
+  async mounted() {
+    let that = this;
+    axios.get("/api/contributors").then((res) => {
+      that.contributors = res.data;
+      console.log(res.data);
+    });
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.about {
+  background-color: rgb(235, 235, 235);
+  height: calc(100vh - 50px);
+  width: 100vw;
+}
+.content {
+  margin-left: 40px;
+  margin-right: 40px;
+  background-color: whitesmoke;
+  height: calc(100vh - 70px);
+  border-bottom-left-radius: 15px;
+  border-bottom-right-radius: 15px;
+}
+.holder {
+  padding: 10px;
+}
+.section-title {
+  font-size: 35px;
+}
+
+.section {
+  display: flex;
+  padding-top: 25px;
+  flex-wrap: wrap;
+  justify-content: space-evenly;
+  align-items: center;
+}
+</style>
